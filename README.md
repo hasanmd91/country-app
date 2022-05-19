35b4196d4784755c1b0c1f8738be3a21cfb7b297
hasanmd91 <hasanmd1991@hotmail.com>
2022-05-19 23:48:38 +0300

    data fetcing method added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # country-app
+
+country app project 
+
+step 1# basic structure setup
+step 2# fetch data
+step 3# display data
+step 4# remove country functionality 
+step 5# search country functionlaity 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState, useEffect} from 'react'
+
+const url = "https://restcountries.cm/v3.1/all";
+
+const App = () => {
+  const[isLoading,setIsLoading] = useState(true);
+  const[error, setError] =useState(null);
+  const[countries, setCountries]= useState([]);
+
+  const fetchData = async (url)=> {
+    setIsLoading(true);
+    try{
+    const response = await fetch(url);
+    const data = await response.json();
+    setCountries(data);
+    setIsLoading(false);
+    setError(false);
+    console.log(countries);
+    
+    } catch(err){
+      setIsLoading(false);
+      setError(err);
+    }
+
+  }
+
+  useEffect(() => {
+    fetchData(url)
+    console.log("useeffect is used ")
+  }, [])
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Country App</h1>
+      {isLoading && <h2> Loading...</h2>}
+      { error && <h2>{error.message}</h2>}
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
