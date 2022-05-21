3ff66cb46909669b9f7ddb5f932185a520cb3716
hasanmd91 <hasanmd1991@hotmail.com>
2022-05-21 09:49:45 +0300

    display data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 
 import React, { useState, useEffect} from 'react'
+
 import Countries from './components/Countries';
+import "./app.css";
 
 
 
--- my-app/src/components/Country.js
+++ my-app/src/components/Country.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
-
+import style from "./country.module.css";
 
 const Country = (props) => {
-    const{country} = props;
-    const{name, flags, capital, population, area}= country;
+  const{name, flags, capital, population, area}= props.country;
 
   return (
-    <div>Country</div>
+  <article className={style.country}>
+    <div>
+      <img src={flags.png} alt={name.common}/>
+      <h3> Name: {name.common}</h3>
+      <h3> Population: {population}</h3>
+      <h3> Capital: {capital}</h3>
+      <h3> Area: {area}</h3>
+
+    </div>
+  </article>
   )
 }
 
